b697e544245c86c7e7b34f02f9493a9b6a850c58
Ervis Trupja <ervistrupja@outlook.com>
2025-04-07 17:01:14 +0200

    Add m0410b exercise files

---
+++ Expenses.Client/src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Observable, tap } from 'rxjs';
+import { AuthResponse } from '../models/authresponse';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'https://localhost:7145/api/Auth';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(credentials: User): Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(this.apiUrl+"/Login", credentials)
+                    .pipe(
+                      tap((response) => {
+                        localStorage.setItem('token', response.token);
+                      })
+                    )
+  }
+
+  register(credentials: User): Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(this.apiUrl+"/Register", credentials)
+                    .pipe(
+                      tap((response) => {
+                        localStorage.setItem('token', response.token);
+                      })
+                    )
+  }
+  
+  logout():void {
+    localStorage.removeItem("token");
+    this.router.navigate(['/login']);
+  }
+
+  isAuthenticated() : boolean {
+    return !!localStorage.getItem('token');
+  }
+
+}
