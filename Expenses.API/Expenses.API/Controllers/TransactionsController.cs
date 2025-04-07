c064a0762f10491ce6c0113e8b1b58d1f367978d
Ervis Trupja <ervistrupja@outlook.com>
2025-04-07 17:00:26 +0200

    Add m0208b exercise files

---
+++ Expenses.API/Expenses.API/Controllers/TransactionsController.cs
@@ -0,0 +1,68 @@
+﻿using Expenses.API.Data;
+using Expenses.API.Dtos;
+using Expenses.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("All")]
+        public IActionResult GetAll()
+        {
+            var allTransactions = context.Transactions.ToList();
+            return Ok(allTransactions);
+        }
+
+        [HttpGet("Details/{id}")]
+        public IActionResult Get(int id)
+        {
+            var transactionDb = context.Transactions.FirstOrDefault(n => n.Id == id);
+            if (transactionDb == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(transactionDb);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult CreateTransaction([FromBody]PostTransactionDto payload)
+        {
+            var newTransaction = new Transaction()
+            {
+                Amount = payload.Amount,
+                Type = payload.Type,
+                Category = payload.Category,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            context.Transactions.Add(newTransaction);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut("Update/{id}")]
+        public IActionResult UpdateTransaction(int id, [FromBody]PutTransactionDto payload)
+        {
+            var transactionDb = context.Transactions.FirstOrDefault(n=>n.Id == id);
+            if (transactionDb == null)
+                return NotFound();
+
+            transactionDb.Type = payload.Type;
+            transactionDb.Amount = payload.Amount;
+            transactionDb.Category = payload.Category;
+            transactionDb.UpdatedAt = DateTime.UtcNow;
+
+            context.Transactions.Update(transactionDb);
+            context.SaveChanges();
+
+            return Ok(transactionDb);
+        }
+    }
+}
