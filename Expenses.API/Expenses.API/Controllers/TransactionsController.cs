b16fb45416ae20cb15b7e84ddf3fa185b50fe583
Ervis Trupja <ervistrupja@outlook.com>
2025-04-07 17:00:20 +0200

    Add m0205b exercise files

---
+++ Expenses.API/Expenses.API/Controllers/TransactionsController.cs
@@ -0,0 +1,32 @@
+﻿using Expenses.API.Data;
+using Expenses.API.Dtos;
+using Expenses.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController(AppDbContext context) : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult CreateTransaction([FromBody]PostTransactionDto payload)
+        {
+            var newTransaction = new Transaction()
+            {
+                Amount = payload.Amount,
+                Type = payload.Type,
+                Category = payload.Category,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            context.Transactions.Add(newTransaction);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
